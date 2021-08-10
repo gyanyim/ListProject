6cda2feafee71971ec0adbcd8a0100c9f9ea7bf1
Miklos_Gyanyi <Miklos_Gyanyi@epam.com>
2021-08-10 15:29:07 +0200

    Added Spring Validator to ArrayService

--- src/main/java/com/example/listarrayproject/service/ArrayService.java
+++ src/main/java/com/example/listarrayproject/service/ArrayService.java
@@ -5,12 +5,39 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
+import javax.xml.bind.ValidationException;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class ArrayService implements Validator {
+
+    public List<Integer> getFlattenedList(List<List<Integer>> nestedList) throws ValidationException {
+        validateList(nestedList);
+        return makeFlattenedList(nestedList);
+    }
+
+    private void validateList(List<List<Integer>> nestedList) throws ValidationException {
+        if (nestedList.isEmpty()) {
+            throw new ValidationException("The nestedList is empty!");
+        }
+
+        List<Integer> flattenedList = makeFlattenedList(nestedList);
+
+        flattenedList.forEach(e -> {
+            if (e == null) {
+                throw new NullPointerException("The nestedList contains null!");
+            }
+        });
+    }
+
+    private List<Integer> makeFlattenedList(List<List<Integer>> nestedList) {
+        return nestedList.stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+    }
+
 
     @Override
     public boolean supports(Class aClass) {
@@ -29,17 +56,4 @@ public class ArrayService implements Validator {
             }
         });
     }
-
-    public List<Integer> getFlattenedList(List<List<Integer>> nestedList) {
-        return makeFlattenedList(nestedList);
-    }
-
-    private List<Integer> makeFlattenedList(List<List<Integer>> nestedList) {
-        return nestedList.stream()
-                .flatMap(Collection::stream)
-                .collect(Collectors.toList());
-    }
-
-
-
 }
